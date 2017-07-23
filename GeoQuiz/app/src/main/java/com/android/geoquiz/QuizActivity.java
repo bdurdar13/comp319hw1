4c03c334d2ed645a52d1a68912c7a837eb634ea0
Elçin Candoğan <mcandogan13@ku.edu.tr>
2017-07-23 14:33:07 +0300

    bi tek timerlar kaldi

--- GeoQuiz/app/src/main/java/com/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/android/geoquiz/QuizActivity.java
@@ -1,9 +1,11 @@
 package com.android.geoquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,7 +30,7 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
 
-        // mScoreView =(TextView) findViewById(R.id.score);
+        mScoreView =(TextView) findViewById(R.id.score);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
 
@@ -45,6 +47,8 @@ public class QuizActivity extends AppCompatActivity {
                     mScore = mScore - 20;
                     Toast.makeText(QuizActivity.this, "INCORRECT!", Toast.LENGTH_LONG)
                             .show();
+                    updateScore();
+
                 }
             }
         });
@@ -56,13 +60,15 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (mBButton.getText() == mAnswer) {
                     mScore = mScore + 100;
-                    //  updateScore(mScore);
+                    updateScore();
                     Toast.makeText(QuizActivity.this, "CORRECT!", Toast.LENGTH_LONG)
                             .show();
                 } else {
                     mScore = mScore - 20;
                     Toast.makeText(QuizActivity.this, "INCORRECT!", Toast.LENGTH_LONG)
                             .show();
+                    updateScore();
+
                 }
             }
         });
@@ -74,13 +80,15 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (mCButton.getText() == mAnswer) {
                     mScore = mScore + 100;
-                    //  updateScore(mScore);
+                    updateScore();
                     Toast.makeText(QuizActivity.this, "CORRECT!", Toast.LENGTH_LONG)
                             .show();
                 } else {
                     mScore = mScore - 20;
                     Toast.makeText(QuizActivity.this, "INCORRECT!", Toast.LENGTH_LONG)
                             .show();
+                    updateScore();
+
                 }
             }
         });
@@ -91,26 +99,42 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (mDButton.getText() == mAnswer) {
                     mScore = mScore + 100;
-                    //  updateScore(mScore);
+                    updateScore();
                     Toast.makeText(QuizActivity.this, "CORRECT!", Toast.LENGTH_LONG)
                             .show();
                 } else {
                     mScore = mScore - 20;
                     Toast.makeText(QuizActivity.this, "INCORRECT!", Toast.LENGTH_LONG)
                             .show();
+                    updateScore();
+
                 }
             }
         });
 
+        if(mCurrentIndex<10) {
+            mNextButton = (Button) findViewById(R.id.next_button);
+            mNextButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateQuestion();
 
-        mNextButton = (Button) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-            public void onClick(View v) {
-               updateQuestion();
+                }
+            });
+        }else {
+            Button finalNext = (Button) findViewById(R.id.next_button);
+            finalNext.setOnClickListener(new View.OnClickListener() {
 
-           }
-        });
+                @Override
+                public void onClick(View view) {
+
+                    Intent startInt = new Intent(QuizActivity.this, ScoreActivity.class);
+                    startActivity(startInt);
+                }
+
+            });
+        }
+
     }
 
     private void updateScore(){
